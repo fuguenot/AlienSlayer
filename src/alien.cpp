201c05cecf836d325f677ca1dfbbf1d9a1f5a925
fuguenot <fuguenot@gmail.com>
2024-11-23 10:56:51 -0800

    Update difficulty scaling

--- src/alien.cpp
+++ src/alien.cpp
@@ -19,7 +19,7 @@ as::Alien as::Alien::spawn(SDL_Texture *tex,
                            float y,
                            unsigned int difficulty) {
     float theta = rand_float(-PI / 2, PI / 2);
-    float v = rand_float(0, 0.5) + difficulty * 0.5;
+    float v = rand_float(0, 0.5) + difficulty * 0.25;
     return {tex, x, y, v * std::cosf(theta), v * std::sinf(theta)};
 }
 
