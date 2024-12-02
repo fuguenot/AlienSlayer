73f98c0185e6e32e037c2ea1a74a3aafb1b11e52
fuguenot <fuguenot@gmail.com>
2024-12-02 11:04:13 -0800

    Add invaderside

--- src/sound.cpp
+++ src/sound.cpp
@@ -6,7 +6,7 @@ as::SoundManager::SoundManager() noexcept {
 }
 
 void as::SoundManager::init() {
-    if ((menu_ost = Mix_LoadMUS("../Resources/powerside.ogg")) == nullptr)
+    if ((menu_ost = Mix_LoadMUS("../Resources/invaderside.ogg")) == nullptr)
         throw Error::sdl_mixer("loading menu OST");
     Mix_VolumeMusic(64);
 }
